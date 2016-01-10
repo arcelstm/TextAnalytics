ec9a0d91c0c333f58e3a5605e29d9a86e518e1a3
marc1pan <marc1pan@marc1pan-PC.fritz.box>
2016-01-10 02:14:01 +0100

    x

--- de.unidue.langtech.teaching.pp.stacha/src/main/java/de/unidue/langtech/teaching/pp/stacha/pipeline/BasicPipeline.java
+++ de.unidue.langtech.teaching.pp.stacha/src/main/java/de/unidue/langtech/teaching/pp/stacha/pipeline/BasicPipeline.java
@@ -5,14 +5,9 @@ import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
 
-import org.apache.uima.analysis_component.AnalysisComponent;
-
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.StopWord;
 import de.tudarmstadt.ukp.dkpro.core.snowball.SnowballStemmer;
 import de.tudarmstadt.ukp.dkpro.core.stopwordremover.StopWordRemover;
 import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
-import de.unidue.langtech.teaching.pp.ownReaderTest.NewReaderSolution;
-import de.unidue.langtech.teaching.pp.ownReaderTest.NewReaderTest;
 import de.unidue.langtech.teaching.pp.stacha.BaselineExample;
 import de.unidue.langtech.teaching.pp.stacha.EvaluatorExample;
 import de.unidue.langtech.teaching.pp.stacha.ReaderExample;
@@ -21,9 +16,7 @@ import de.unidue.langtech.teaching.pp.stacha.newType.LetterAnnotator;
 public class BasicPipeline{
 	
     public static void main(String[] args) throws Exception{
-    	
-  
-    	
+    	    	
         SimplePipeline.runPipeline(
         		
                 CollectionReaderFactory.createReader(
@@ -36,7 +29,7 @@ public class BasicPipeline{
                 AnalysisEngineFactory.createEngineDescription(LetterAnnotator.class),
                 AnalysisEngineFactory.createEngineDescription(EvaluatorExample.class),
                 AnalysisEngineFactory.createEngineDescription(SnowballStemmer.class,SnowballStemmer.PARAM_LANGUAGE,"en"),
-               // AnalysisEngineFactory.createEngineDescription(CasDumpWriter.class),
+                //AnalysisEngineFactory.createEngineDescription(CasDumpWriter.class),
                 AnalysisEngineFactory.createEngineDescription(StopWordRemover.class, StopWordRemover.PARAM_MODEL_LOCATION,"src/main/resources/txt/stop.txt")
         );
                 
