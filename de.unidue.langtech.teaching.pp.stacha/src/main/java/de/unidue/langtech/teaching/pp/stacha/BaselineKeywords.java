83d708002c99654d06c1ea4ea4d97e72fc7776ff
marc1pan <marc1pan@marc1pan-PC.fritz.box>
2016-02-03 01:00:05 +0100

    token =/= keyphrase
    token cast funktioniert nicht
    getScore() funktioniert nicht

--- de.unidue.langtech.teaching.pp.stacha/src/main/java/de/unidue/langtech/teaching/pp/stacha/BaselineKeywords.java
+++ de.unidue.langtech.teaching.pp.stacha/src/main/java/de/unidue/langtech/teaching/pp/stacha/BaselineKeywords.java
@@ -1,17 +1,17 @@
 package de.unidue.langtech.teaching.pp.stacha;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
-import org.apache.uima.cas.Type;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
 
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.unidue.langtech.teaching.pp.type.DetectedLanguage;
+import de.unidue.langtech.teaching.pp.type.Keyphrase;
 
 public class BaselineKeywords extends JCasAnnotator_ImplBase{
 
@@ -20,22 +20,43 @@ public class BaselineKeywords extends JCasAnnotator_ImplBase{
 	(name= PARAM_LANGUAGE, mandatory=false, defaultValue= "en")
 	protected String language;
 	
+	@SuppressWarnings("deprecation")
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
 
-		System.out.println("Document is: " + jcas.getDocumentText()+"\n");
+		System.out.println("Document is: " + jcas.getDocumentText());
         
         Collection<Token> tokens = JCasUtil.select(jcas, Token.class);
         DetectedLanguage languageAnno = new DetectedLanguage(jcas);
 		
-        int i=0;
-        for(Token t:tokens){
-        	if(t.getEnd()-t.getStart()>1)
-        //	if(t.getPos().getPosValue().contains("NN")){
-        		System.out.println("(" + t.getPos().getPosValue()+") " + t.getCoveredText() +"\n---");
-        		i++;
-        //	}
+//        int i=0;
+//        for(Token t:tokens){
+//        	if(t.getEnd()-t.getStart()>1)
+//        		if(t.getPos().getPosValue().contains("NN")){
+//        			System.out.println("(" + t.getPos().getPosValue()+") " + t.getCoveredText() +"\n---");
+//        			i++;
+//        		}
+//        }
+        
+        for(int i=0; i <tokens.size(); i++){
+        	Annotation x= (Annotation) tokens.toArray()[i];
+        	
+        	if(x.getEnd()-x.getStart()>1)
+        		
+        	if(	((Token) x).getPos().getPosValue().contains("NN") || 
+        		((Token) x).getPos().getPosValue().contains("JJ") || 
+        		((Token) x).getPos().getPosValue().contains("V")){
+        		System.out.println("---\n(" + ((Token) x).getPos().getPosValue()+") " + x.getCoveredText());
+        	}
+        	
+        	//Keyphrase y = null;
+			//Keyphrase y = x.getCoveredText();
+        	//System.out.println(y.getScore());
+        	
+        	i++;
         }
+        
+        
         
         languageAnno.setLanguage(language);    
         languageAnno.addToIndexes();
