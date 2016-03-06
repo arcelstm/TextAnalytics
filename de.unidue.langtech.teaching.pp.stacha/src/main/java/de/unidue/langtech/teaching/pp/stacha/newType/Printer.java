bd4ae02ebb51f84e15b9cd03faacb34990495a96
marc1pan <marc1pan@marc1pan-PC.fritz.box>
2016-03-06 21:43:07 +0100

    sorted top 15 Keyphrases

--- de.unidue.langtech.teaching.pp.stacha/src/main/java/de/unidue/langtech/teaching/pp/stacha/newType/Printer.java
+++ de.unidue.langtech.teaching.pp.stacha/src/main/java/de/unidue/langtech/teaching/pp/stacha/newType/Printer.java
@@ -1,21 +1,17 @@
 package de.unidue.langtech.teaching.pp.stacha.newType;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.TreeMap;
 
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.jcas.tcas.Annotation;
 
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.keyphrases.core.type.Keyphrase;
 
 public class Printer
@@ -27,32 +23,37 @@ public class Printer
         throws AnalysisEngineProcessException
     { 
     	Collection<Keyphrase> keyphrases = JCasUtil.select(jcas, Keyphrase.class);
-    	Collection<Token> tokens = JCasUtil.select(jcas, Token.class);
+    	//Collection<Token> tokens = JCasUtil.select(jcas, Token.class);
     	Collection<Sentence> sentences = JCasUtil.select(jcas, Sentence.class);
-    	Map<String,Number> map = new HashMap<String, Number>();
-
+    	Map<Number,String> map = new TreeMap<Number,String>(Collections.reverseOrder());
+    	
+    	
+    	System.out.println("");
+    	
     	 for(Keyphrase k:keyphrases){
     		//if(k.getScore()>0 && !(k.getCoveredText().contains(".")))
     		//if(k.getScore()>1 && (k.getEnd()-k.getStart())>7)
-    		 map.put(k.getKeyphrase(),k.getScore());
+    		
+    		 map.put(Math.floor(k.getScore()*100)/100,k.getKeyphrase());
   		 }
     	 
+    	 for (Sentence s : sentences) System.out.println(s.getCoveredText());
+    	 System.out.println("");
     	 
-
-    		 for(Entry<String, Number> e: map.entrySet()){
-    			 System.out.println(e);
-	    		//System.out.println(k.getKeyphrase()+ " - " + k.getScore());
-    	}
+    	 int index=0;
     	
+    	 for(Entry<Number,String> e: map.entrySet()){
+    		 System.out.println(e.getValue() + " ### " + e.getKey());
+    		 index++;
+    		if (index>=15) break;
+    	 }
     	    	
-        for (Sentence s : sentences) { 
-        	//System.out.println(s.getCoveredText());
-        }	
-        
-        	for (Token t:tokens){
-        		if(t.getPos().getPosValue().contains("NN")||t.getPos().getPosValue().contains("VB")||t.getPos().getPosValue().contains("J")){
+    	 System.out.println("");
+    	 
+       // 	for (Token t:tokens){
+      //  		if(t.getPos().getPosValue().contains("NN")||t.getPos().getPosValue().contains("VB")||t.getPos().getPosValue().contains("J")){
         		//	System.out.println(t.getCoveredText()); 
-        		}
+      //  		}
         		
        
     	
@@ -60,5 +61,3 @@ public class Printer
     	
         
     }
-
-}
--- de.unidue.langtech.teaching.pp.stacha/src/main/java/de/unidue/langtech/teaching/pp/stacha/pipeline/KeyPipeline.java
+++ de.unidue.langtech.teaching.pp.stacha/src/main/java/de/unidue/langtech/teaching/pp/stacha/pipeline/KeyPipeline.java
@@ -1,60 +1,19 @@
 package de.unidue.langtech.teaching.pp.stacha.pipeline;
 
-import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
-
-import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
-import org.apache.uima.fit.util.JCasUtil;
 
-import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.ADJ;
-import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.ADJ_Type;
-import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.N;
-import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.NN;
-import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
-import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpNameFinder;
 import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger;
-import de.tudarmstadt.ukp.dkpro.core.snowball.SnowballStemmer;
-import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordNamedEntityRecognizer;
-import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordParser;
 import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter;
-import de.tudarmstadt.ukp.dkpro.core.stopwordremover.StopWordRemover;
-import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
 import de.tudarmstadt.ukp.dkpro.keyphrases.core.candidate.CandidateAnnotator;
-import de.tudarmstadt.ukp.dkpro.keyphrases.core.candidate.CandidateAnnotatorFactory;
-import de.tudarmstadt.ukp.dkpro.keyphrases.core.coreference.ranking.CoreferencedTfidfAnnotator;
-import de.tudarmstadt.ukp.dkpro.keyphrases.core.evaluator.KeyphraseEvaluator;
-import de.tudarmstadt.ukp.dkpro.keyphrases.core.evaluator.KeyphraseGoldStandardFilter;
-import de.tudarmstadt.ukp.dkpro.keyphrases.core.evaluator.KeyphraseWriter;
-import de.tudarmstadt.ukp.dkpro.keyphrases.core.evaluator.util.Matchings;
 import de.tudarmstadt.ukp.dkpro.keyphrases.core.filter.PosSequenceFilter;
-import de.tudarmstadt.ukp.dkpro.keyphrases.core.filter.factory.KeyphraseMergerFactory;
 import de.tudarmstadt.ukp.dkpro.keyphrases.core.postprocessing.KeyphraseMerger;
-import de.tudarmstadt.ukp.dkpro.keyphrases.core.ranking.PositionRanking;
-import de.tudarmstadt.ukp.dkpro.keyphrases.core.ranking.RandomRanking;
-import de.tudarmstadt.ukp.dkpro.keyphrases.core.ranking.TfidfRanking;
 import de.tudarmstadt.ukp.dkpro.keyphrases.core.reader.KeyphraseReader;
-import de.tudarmstadt.ukp.dkpro.keyphrases.core.type.Keyphrase;
-import de.tudarmstadt.ukp.dkpro.keyphrases.core.util.KeyphraseScoreComparator;
-import de.tudarmstadt.ukp.dkpro.keyphrases.core.util.KeyphraseUtil;
-import de.tudarmstadt.ukp.dkpro.keyphrases.core.wrapper.Candidate;
-import de.tudarmstadt.ukp.dkpro.keyphrases.core.wrapper.CooccurrenceGraphExtractor;
-import de.tudarmstadt.ukp.dkpro.keyphrases.core.wrapper.KeyphraseExtractor;
-import de.tudarmstadt.ukp.dkpro.keyphrases.ranking.NodeDegreeRanking;
 import de.tudarmstadt.ukp.dkpro.keyphrases.ranking.PageRankRanking;
 import de.tudarmstadt.ukp.dkpro.keyphrases.textgraphs.CooccurrenceGraph;
-import de.tudarmstadt.ukp.dkpro.keyphrases.textgraphs.CooccurrenceGraphFactory;
-import de.tudarmstadt.ukp.dkpro.keyphrases.textgraphs.util.PageRank;
-import de.unidue.langtech.teaching.pp.stacha.BaselineKeywords;
 import de.unidue.langtech.teaching.pp.stacha.newType.Printer;
-import de.unidue.langtech.teaching.pp.stacha.newType.Test;
-import de.unidue.langtech.teaching.pp.type.DetectedLanguage;
-import edu.stanford.nlp.dcoref.CoNLL2011DocumentReader.NamedEntityAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
-import net.didion.jwnl.data.Adjective;
-import net.didion.jwnl.data.list.Node;
 
 public class KeyPipeline {
 
@@ -67,22 +26,22 @@ public class KeyPipeline {
                         KeyphraseReader.PARAM_INPUTDIR, "src/test/resources/txt",
                         KeyphraseReader.PARAM_DATA_SUFFIX, "txt"
                 )
-                
-               ,AnalysisEngineFactory.createEngineDescription(StanfordSegmenter.class, StanfordSegmenter.PARAM_LANGUAGE, "en",StanfordSegmenter.PARAM_STRICT_ZONING, false) //TOKENIZATION  
-                      
-               ,AnalysisEngineFactory.createEngineDescription(OpenNlpPosTagger.class,OpenNlpPosTagger.PARAM_LANGUAGE, "en")
-               
-          	,AnalysisEngineFactory.createEngineDescription(CooccurrenceGraph.class,CooccurrenceGraph.PARAM_FEATURE_PATH,Token.class,CooccurrenceGraph.PARAM_WINDOW_SIZE,5)
-            ,AnalysisEngineFactory.createEngineDescription(PageRankRanking.class,PageRankRanking.PARAM_WEIGHTED,true)
-           	  ,AnalysisEngineFactory.createEngineDescription(CandidateAnnotator.class, CandidateAnnotator.PARAM_FEATURE_PATH, Token.class)
-           	  ,AnalysisEngineFactory.createEngineDescription(PosSequenceFilter.class)
 
+            ,AnalysisEngineFactory.createEngineDescription(StanfordSegmenter.class, StanfordSegmenter.PARAM_LANGUAGE, "en",StanfordSegmenter.PARAM_STRICT_ZONING, false) //TOKENIZATION  
+
+            ,AnalysisEngineFactory.createEngineDescription(OpenNlpPosTagger.class,OpenNlpPosTagger.PARAM_LANGUAGE, "en")
+
+          	,AnalysisEngineFactory.createEngineDescription(CooccurrenceGraph.class,CooccurrenceGraph.PARAM_FEATURE_PATH,Token.class,CooccurrenceGraph.PARAM_WINDOW_SIZE,5)
+              ,AnalysisEngineFactory.createEngineDescription(PageRankRanking.class,PageRankRanking.PARAM_WEIGHTED, false)              
+
+            ,AnalysisEngineFactory.createEngineDescription(CandidateAnnotator.class, CandidateAnnotator.PARAM_FEATURE_PATH, Token.class)
+          
+           	,AnalysisEngineFactory.createEngineDescription(PosSequenceFilter.class)
            //	,CooccurrenceGraphFactory.getCooccurrenceGraph_token(),createEngineDescription(PageRankRanking.class,PageRankRanking.PARAM_WEIGHTED, true)
            	            	
-           	  ,AnalysisEngineFactory.createEngineDescription(KeyphraseMerger.class) 
-           	  ,AnalysisEngineFactory.createEngineDescription(StopWordRemover.class, StopWordRemover.PARAM_MODEL_LOCATION,"[en]src/test/resources/stopwords/stop.txt")
+           	,AnalysisEngineFactory.createEngineDescription(KeyphraseMerger.class)
+           	,AnalysisEngineFactory.createEngineDescription(Printer.class)
 
-           	  ,AnalysisEngineFactory.createEngineDescription(Printer.class)
          );
 		
 		//AnalysisEngineDescription graph = createEngineDescription(CooccurrenceGraphFactory.getCooccurrenceGraph_token(),createEngineDescription(PageRankRanking.class, PageRankRanking.PARAM_WEIGHTED, true));
