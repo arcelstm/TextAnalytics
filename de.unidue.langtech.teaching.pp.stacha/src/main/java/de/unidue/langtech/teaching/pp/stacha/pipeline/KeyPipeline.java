f123b17f3905bfc9dac6934869c04bd2c1a6d80c
marc1pan <marc1pan@marc1pan-PC.fritz.box>
2016-03-10 23:55:18 +0100

    using TextReader instead of KeyphraseReader

--- de.unidue.langtech.teaching.pp.stacha/src/main/java/de/unidue/langtech/teaching/pp/stacha/pipeline/KeyPipeline.java
+++ de.unidue.langtech.teaching.pp.stacha/src/main/java/de/unidue/langtech/teaching/pp/stacha/pipeline/KeyPipeline.java
@@ -5,12 +5,14 @@ import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
 
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import de.tudarmstadt.ukp.dkpro.core.io.text.TextReader;
 import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger;
 import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter;
 import de.tudarmstadt.ukp.dkpro.core.stopwordremover.StopWordRemover;
 import de.tudarmstadt.ukp.dkpro.keyphrases.bookindexing.aggregation.RankedPhraseAggregationAnnotator;
 import de.tudarmstadt.ukp.dkpro.keyphrases.core.candidate.CandidateAnnotator;
 import de.tudarmstadt.ukp.dkpro.keyphrases.core.evaluator.KeyphraseEvaluator;
+import de.tudarmstadt.ukp.dkpro.keyphrases.core.evaluator.KeyphraseWriter;
 import de.tudarmstadt.ukp.dkpro.keyphrases.core.filter.CorpusFilter;
 import de.tudarmstadt.ukp.dkpro.keyphrases.core.filter.KeyphraseFilter;
 import de.tudarmstadt.ukp.dkpro.keyphrases.core.filter.PosSequenceFilter;
@@ -30,35 +32,51 @@ public class KeyPipeline {
 		String goldSuffix = null;
 		SimplePipeline.runPipeline(
         		
-                CollectionReaderFactory.createReader(
+				CollectionReaderFactory.createReader(
+						
+						TextReader.class,
+						TextReader.PARAM_SOURCE_LOCATION, "src/test/resources/txt/test.txt",
+						TextReader.PARAM_LANGUAGE,"en"
+						
+						)
+				
+                /*CollectionReaderFactory.createReader(
                         KeyphraseReader.class,
                         KeyphraseReader.PARAM_INPUTDIR, "src/test/resources/txt",
                         KeyphraseReader.PARAM_DATA_SUFFIX, "txt",
                         KeyphraseReader.PARAM_LANGUAGE, "en"
-                )
+                )*/
+                
             ,AnalysisEngineFactory.createEngineDescription(StanfordSegmenter.class) //TOKENIZATION  
 
             ,AnalysisEngineFactory.createEngineDescription(OpenNlpPosTagger.class)
+            ,AnalysisEngineFactory.createEngineDescription(StopWordRemover.class,StopWordRemover.PARAM_MODEL_LOCATION,"src/test/resources/stopwords/stop.txt")
 
           	,AnalysisEngineFactory.createEngineDescription(CooccurrenceGraph.class,CooccurrenceGraph.PARAM_FEATURE_PATH,Token.class,CooccurrenceGraph.PARAM_WINDOW_SIZE,5)
             ,AnalysisEngineFactory.createEngineDescription(PageRankRanking.class,PageRankRanking.PARAM_WEIGHTED, false)              
 
             ,AnalysisEngineFactory.createEngineDescription(CandidateAnnotator.class, CandidateAnnotator.PARAM_FEATURE_PATH, Token.class)
-           	,AnalysisEngineFactory.createEngineDescription(PosSequenceFilter.class)
            //	,CooccurrenceGraphFactory.getCooccurrenceGraph_token(),createEngineDescription(PageRankRanking.class,PageRankRanking.PARAM_WEIGHTED, true)
-           	
+
            	,AnalysisEngineFactory.createEngineDescription(KeyphraseMerger.class, KeyphraseMerger.PARAM_KEEP_PARTS, false, KeyphraseMerger.PARAM_MAX_LENGTH,4)
-         //  	,AnalysisEngineFactory.createEngineDescription(PostProcessing.class)
-           /*	,AnalysisEngineFactory.createEngineDescription(
+,AnalysisEngineFactory.createEngineDescription(PosSequenceFilter.class)
+           
+
+        	,AnalysisEngineFactory.createEngineDescription(
+        			KeyphraseWriter.class, KeyphraseWriter.PARAM_N, 10, 
+        			KeyphraseWriter.PARAM_LOWERCASE,true,
+        			KeyphraseWriter.PARAM_REMOVE_CONTAINED,true
+        	)
+        	
+        	,AnalysisEngineFactory.createEngineDescription(
                     KeyphraseEvaluator.class,
-                    KeyphraseEvaluator.PARAM_N, 0,
+                    KeyphraseEvaluator.PARAM_N, 10,
                     KeyphraseEvaluator.PARAM_EVAL_TYPE, KeyphraseEvaluator.EvaluatorType.Token.toString(),
                     KeyphraseEvaluator.PARAM_REMOVE_GOLD_AFTER_MATCH, true,
                     KeyphraseEvaluator.PARAM_GOLD_SUFFIX, goldSuffix,
                     KeyphraseEvaluator.PARAM_LOWERCASE,true
-            )*/
-           	
-           	,AnalysisEngineFactory.createEngineDescription(Printer.class)
+            )
+           	//,AnalysisEngineFactory.createEngineDescription(Printer.class)
 
          );
 		
